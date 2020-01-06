d4452e4a0390e86af492a4006fa25e5eb2c02c4d
Austin Asbury <acasbury25@aol.com>
2020-01-06 02:52:06 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,9 +3,4 @@ class Student < ActiveRecord::Base
     self.first_name + " " + self.last_name
   end
 
-  def is_active?
-    if self.active == false
-      "This student is currently inactive."
-    end
-  end
 end
